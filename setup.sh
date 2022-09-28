cb6660d90261c03794d1ce34e75518db839cc460
pawanshaw66 <97239563+pawanshaw66@users.noreply.github.com>
2022-09-28 20:14:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"heroku logs --tail\"\n\
-">~/.streamlit/credential.toml
+
 echo "\
 [server]\n\
-headless = false\n\
 port = $PORT\n\
-">~/.streamlit/config.toml
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
